7346522fcdc45bdacc527de65fe3896b948cab0d
Zachary Kanfer <zkanfer@gmail.com>
2014-03-27 00:25:20 -0400

    Add GPLv3.

--- 2048.el
+++ 2048.el
@@ -2,6 +2,25 @@
 
 ;; Copyright 2014 Zachary Kanfer
 
+;; Author: Zachary Kanfer <zkanfer@gmail.com>
+;; Version: 2014.03.27
+;; URL: https://bitbucket.org/zck/2048.el
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (defun 2048-game () "Major mode for playing 2048 in Emacs"
   (interactive)
   (switch-to-buffer "2048")
